dbfdbba54ec77a79b76d23a9b6f6cef200631349
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-21 07:57:31 -0300

    refactor: create user

--- src/context/users/createUser/createUser.service.ts
+++ src/context/users/createUser/createUser.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, ConflictException } from '@nestjs/common';
 import { CreateUserDTO } from 'src/shared/dto/createUser.dto';
 import { User } from 'src/shared/entities/user.entity';
+import { Hasher } from 'src/shared/providers/EncryptProvider/protocols/hasher';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,10 +9,27 @@ export class CreateUserService {
   constructor(
     @Inject('USER_REPOSITORY')
     private userRepository: Repository<User>,
+    @Inject('HASH_PROVIDER')
+    private hasher: Hasher,
   ) {}
 
   async create(data: CreateUserDTO): Promise<User> {
-    const user = this.userRepository.create(data);
+    const savedUser = await this.userRepository.query(
+      `SELECT first_name FROM users WHERE email = $1`,
+      [data.email],
+    );
+    if (savedUser.length > 0) {
+      throw new ConflictException(
+        'E-mail already in use! Try to recover your password',
+      );
+    }
+
+    const hashedPassword = this.hasher.createHash(data.password);
+
+    const user = this.userRepository.create({
+      ...data,
+      password: hashedPassword,
+    });
     await this.userRepository.save(user);
 
     return user;
--- src/context/users/user.module.ts
+++ src/context/users/user.module.ts
@@ -1,25 +1,49 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/infra/typeorm/database.module';
+import { JwtProvider } from 'src/shared/providers/CriptographyProvider/jwt.provider';
+import { BcryptProvider } from 'src/shared/providers/EncryptProvider/bcrypt.provider';
 import { CreateUserController } from './createUser/createUser.controller';
 import { CreateUserService } from './createUser/createUser.service';
+import { DeleteUserController } from './deleteUser/deleteUser.controller';
+import { DeleteUserService } from './deleteUser/deleteUser.service';
 import { FindUserByEmailController } from './findUserByEmail/findUserByEmail.controller';
 import { FindUserByEmailService } from './findUserByEmail/findUserByEmail.service';
+import { ListAddressesByUserController } from './listAddressesByUser/listAddressesByUser.controller';
+import { ListAddressesByUserService } from './listAddressesByUser/listAddressesByUser.service';
+import { ListOrdersByUserController } from './listOrdersByUser/listOrdersByUser.controller';
+import { ListOrdersByUserService } from './listOrdersByUser/listOrdersByUser.service';
 import { ListUsersController } from './listUsers/listUsers.controller';
 import { ListUsersService } from './listUsers/listUsers.service';
+import { SigninUserController } from './signinUser/signinUser.controller';
+import { SigninUserService } from './signinUser/signinUser.service';
+import { UpdateUserController } from './updateUser/updateUser.controller';
+import { UpdateUserService } from './updateUser/updateUser.service';
 import { userProviders } from './user.provider';
 
 @Module({
   imports: [DatabaseModule],
   providers: [
+    { provide: 'HASH_PROVIDER', useClass: BcryptProvider },
+    { provide: 'ENCRYPTER_PROVIDER', useClass: JwtProvider },
     ...userProviders,
     CreateUserService,
     FindUserByEmailService,
     ListUsersService,
+    UpdateUserService,
+    DeleteUserService,
+    SigninUserService,
+    ListOrdersByUserService,
+    ListAddressesByUserService,
   ],
   controllers: [
     CreateUserController,
     FindUserByEmailController,
     ListUsersController,
+    UpdateUserController,
+    DeleteUserController,
+    SigninUserController,
+    ListOrdersByUserController,
+    ListAddressesByUserController,
   ],
 })
 export class UserModule {}
