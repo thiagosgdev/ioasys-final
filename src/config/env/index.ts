a36dcfedbef0b17df7db32f27b0ed9d7fe29ee8f
Thiago Gonçalves <thiagosgdev@gmail.com>
2022-02-26 09:51:16 -0300

    chore: dependency installation and configuration

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -5,9 +5,9 @@ export default () => {
     nodeEnv: process.env.NODE_ENV || 'local',
 
     port: parseInt(process.env.PORT, 10),
-    expiresIn: parseInt(process.env.EXPIRES_IN, 10),
     jwtSecret: process.env.JWT_SECRET,
-    jwtExpires: process.env.JWT_EXPIRES,
+    jwtExpires: process.env.JWT_EXPIRES_IN,
+    jwtRefExpires: process.env.JWT_REFRESH_EXP_IN,
     dbHost: process.env.DB_HOST,
     dbPort: parseInt(process.env.DB_PORT, 5432),
     dbUser: process.env.DB_USER,
